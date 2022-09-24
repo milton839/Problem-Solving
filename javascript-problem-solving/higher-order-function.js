44d8e62fbd54eb82b957865e49a3a51639b3829d
Milton839 <miltonbiswas839@gmail.com>
2022-09-24 19:06:01 +0600

    higher order function implemented by two ways

--- javascript-problem-solving/higher-order-function.js
+++ javascript-problem-solving/higher-order-function.js
@@ -1,3 +1,7 @@
+// 1. Function can be passed as an argument
+// 2. Function can be return as an another Function
+
+// 1. Function can be passed as an argument
 function generateTwoRandomNumber(max, cb) {
     const random1 = Math.floor(Math.random() * max);
     const random2 = Math.floor(Math.random() * max);
@@ -15,3 +19,17 @@ generateTwoRandomNumber(100, (random1, random2) => {
 generateTwoRandomNumber(100, (random1, random2) => {
     console.log(random1 - random2);
 })
+
+// 2. Function can be return as an another Function
+function power(p){
+    return function(n){
+        let result = 1;
+        for(let i = 1; i <= p; i++){
+            result = result * n
+        }
+        return result;
+    }
+}
+
+const sqr = power(3);
+console.log(sqr);
